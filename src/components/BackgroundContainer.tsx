9b1aac28c35a47a8f39a14f68cd4d538eb7fc12b
KaSaNaa <pasindudeshitha123@gmail.com>
2025-02-04 23:23:40 +0530

    refactor: simplify BackgroundContainer component by removing unnecessary Container wrapper and adding overflow handling

--- src/components/BackgroundContainer.tsx
+++ src/components/BackgroundContainer.tsx
@@ -1,32 +1,26 @@
-import React from 'react';
-import { Box, Container } from '@mui/material';
+import React from "react";
+import { Box, Container } from "@mui/material";
 
-const BackgroundContainer: React.FC<{ children: React.ReactNode }> = ({ children }) => {
-    return (
-        <Container sx={{
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-            width: '100vw',
-            height: '100vh',
-            margin: 0,
-            padding: 0,
-            background: "red",
-        }}>
-            <Box sx={{
-                width: "100vw",
-                height: "100vh",
-                display: "flex",
-                flexDirection: "column",
-                justifyContent: "center",
-                alignItems: "center",
-                position: "relative",
-                background: "#CF8AB5",
-            }}>
-                {children}
-            </Box>
-        </Container>
-    );
+const BackgroundContainer: React.FC<{ children: React.ReactNode }> = ({
+  children,
+}) => {
+  return (
+    <Box
+      sx={{
+        width: "100vw",
+        height: "100vh",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        position: "relative",
+        overflow: "hidden",
+        background: "#CF8AB5",
+      }}
+    >
+      {children}
+    </Box>
+  );
 };
 
 export default BackgroundContainer;
