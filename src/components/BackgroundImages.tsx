a445d45d7f0c8368eaa3e733432e5dc343156a16
KaSaNaa <pasindudeshitha123@gmail.com>
2025-02-04 23:23:48 +0530

    feat: add BackgroundImages component for decorative visuals in the application

---
+++ src/components/BackgroundImages.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+
+const BackgroundImages: React.FC = () => {
+  return (
+    <>
+      <img
+        src="/src/assets/bow.png"
+        alt="Bow"
+        style={{
+          position: "absolute",
+          top: "-5px",
+          left: "-10px",
+          width: "150px",
+          height: "150px",
+          transform: "rotate(10deg)",
+        }}
+      />
+      <img
+        src="/src/assets/bear.png"
+        alt="Teddy"
+        style={{
+          position: "absolute",
+          top: "50%",
+          right: "-90px",
+          width: "200px",
+          height: "200px",
+        }}
+      />
+      <img
+        src="/src/assets/heart.png"
+        alt="Heart"
+        style={{
+          position: "absolute",
+          bottom: "18px",
+          left: "-20px",
+          width: "250px",
+          height: "250px",
+          transform: "rotate(10deg)",
+        }}
+      />
+    </>
+  );
+};
+
+export default BackgroundImages;
