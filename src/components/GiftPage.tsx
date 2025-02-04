a7183f07c242677b7af60db473f693e33e91a700
KaSaNaa <pasindudeshitha123@gmail.com>
2025-02-04 23:23:53 +0530

    feat: add GiftPage and ValentinePage components for enhanced user interaction and visual storytelling

---
+++ src/components/GiftPage.tsx
@@ -0,0 +1,54 @@
+import React, { useState } from "react";
+import BackgroundContainer from "./BackgroundContainer";
+import BackgroundImages from "./BackgroundImages";
+import { Box, Typography } from "@mui/material";
+import GiftBoxAnimation from "../animation/GiftBoxAnimation";
+
+const GiftPage: React.FC = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleBoxClick = () => {
+    setIsOpen(true);
+  };
+
+  return (
+    <BackgroundContainer>
+      <BackgroundImages />
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center", // Center horizontally
+          justifyContent: "center", // Center vertically
+          height: "100vh", // Full viewport height
+          textAlign: "center", // Center text
+        }}
+      >
+        <Typography
+          variant="h5"
+          sx={{
+            textShadow:
+              "2px 2px 0 #F12585, -2px -2px 0 #F12585, 2px -2px 0 #F12585, -2px 2px 0 #F12585",
+            fontSize: { xs: "1.5rem", sm: "2rem" }, // Responsive font size
+            marginTop: { xs: "-150px", sm: "-200px", md: "-300px" }, // Responsive margin top
+            marginBottom: { xs: "50px", sm: "75px", md: "100px" }, // Responsive margin bottom
+          }}
+        >
+          You have a gift from your chuttaa!
+        </Typography>
+        <Box
+          sx={{
+            position: "relative",
+            width: "200px",
+            height: "200px",
+            cursor: "pointer",
+          }}
+          onClick={handleBoxClick}
+        ><GiftBoxAnimation />
+        </Box>
+      </Box>
+    </BackgroundContainer>
+  );
+};
+
+export default GiftPage;
