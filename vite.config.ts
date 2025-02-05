526c4f10bba894654a59a913e34fe4b3e4fbb115
KaSaNaa <pasindudeshitha123@gmail.com>
2025-02-05 10:51:44 +0530

    Update Vite base path and add deployment scripts in package.json

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/will-u-be-my-valentine/', // Replace with your repository name
+  base: '/Will-You-Be-My-Valentine/',
 });
